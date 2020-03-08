2fb62546ce92ed4b4b60567b6ed8ba197eaf3b9f
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2020-03-08 12:27:07 +0100

    Add missing imports

--- src/scripts/sites/investmentsFinished.js
+++ src/scripts/sites/investmentsFinished.js
@@ -6,7 +6,7 @@
  */
 
 import './investments'
-import {assert, DomMonitor, getCurrencyPrefix, getElementByAttribute, toDate} from '../common/util';
+import {assert, DomMonitor, getCurrencyPrefix, getElementByAttribute, toDate, toFloat} from '../common/util';
 
 chrome.storage.sync.get
 (
